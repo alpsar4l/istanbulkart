d5659f2c15271cc0cda4dfcd2fdd0dd8a11e95f8
Özcan Oktay <ozcanoktay07@gmail.com>
2024-01-08 04:05:53 +0300

    funtion updated

--- utils/extractLastWord.ts
+++ utils/extractLastWord.ts
@@ -1,6 +1,15 @@
-function extractLastWord(text: string) {
-  const splitted_text = text.split("-")
+/**
+ * Extracts the last word from a given text separated by hyphens.
+ * @param {string} text - The input text containing words separated by hyphens.
+ * @returns {string} - The last word extracted from the input text.
+ */
+function extractLastWord(text: string): string {
+  // Split the text into an array using hyphen as the delimiter
+  const splitted_text = text.split("-");
+
+  // Return the last element from the array, which represents the last word
   return splitted_text[splitted_text.length - 1];
 }
 
-export default extractLastWord
+// Export the function as the default export
+export default extractLastWord;
